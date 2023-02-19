ede818f73e72a02bb4226e9e2dfa252fa2408e31
Mark Owings <owingsmk@mail.uc.edu>
2023-02-19 15:14:33 -0500

    sixth commit with fixed code

--- src/FuelCostCalculator.java
+++ src/FuelCostCalculator.java
@@ -1,35 +1,41 @@
-import java.text.DecimalFormat;
+import java.text.DecimalFormat; //decmial format for program
 import java.util.Scanner; //Import to activate scanner function
 public class FuelCostCalculator {
     public static void main(String[] args) {
-
-        DecimalFormat df2 =new DecimalFormat("0.00");
+    DecimalFormat df2 =new DecimalFormat(".00");
     Scanner in = new Scanner(System.in); //Scanner function for the variables
         System.out.print("How many gallons in your gas tank: "); //Output requesting the information for first variable
-        double gas = in.nextDouble(); //Variable for gas
+        int gas = in.nextInt(); //Variable for gas
         System.out.print("What is your fuel efficiency in miles per gallon: ");
         //This variable is for the fuel
         // but it will also be a weird number depending on the person due to mpg
+        double fullGas = (gas*1.00);
         if (gas<1){
             System.out.println(gas+ " is an Invalid Input");
             System.exit(0);
         }
         double milesPerGallon = in.nextDouble(); //Variable for Fuel efficency
         System.out.print("What is the price per gallon right now: ");
+        double mpg = (milesPerGallon*1.00);
         if (milesPerGallon<1){
             System.out.println(milesPerGallon+ " is an Invalid Input");
             System.exit(0);
         }
         double gasPrice = in.nextDouble(); //Variable for gas price
+        double gp = (gasPrice*1.00);
         if (gasPrice<1){
             System.out.println(gasPrice+ " is an Invalid Input");
             System.exit(0);
         }
-        double per100Miles = ((gasPrice*gas)/100); //This is figuring out how much it will cost per 100 miles
-        System.out.println("Your cost per 100 miles is " + per100Miles);
-        double distanceTraveled = ((milesPerGallon/gas)*100); //This is figuring out the distance and mpg and then multiply it all by 100 to get the distance
-        System.out.println("This is the distance you can travel with what is in your tank " + distanceTraveled);
+        double hundered = ((fullGas/mpg)*100);
+            System.out.println("Your cost per 100 miles is " + df2.format(hundered));
 
+
+        double miles = (mpg*0.6214);
+        double gallons = (gp*0.2642);
+        double mg = (miles/gallons);
+
+        System.out.print("Miles/gallons: " + df2.format(mg));
 
     }
 }
