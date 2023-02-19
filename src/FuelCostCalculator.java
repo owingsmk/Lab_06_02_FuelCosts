8cb0e41afebc11294558707a2f708ae801c6d9a6
Mark Owings <owingsmk@mail.uc.edu>
2023-02-19 13:56:28 -0500

    Fifth commit full code

--- src/FuelCostCalculator.java
+++ src/FuelCostCalculator.java
@@ -3,7 +3,7 @@ import java.util.Scanner; //Import to activate scanner function
 public class FuelCostCalculator {
     public static void main(String[] args) {
 
-        DecimalFormat df2 =new DecimalFormat("0.00")
+        DecimalFormat df2 =new DecimalFormat("0.00");
     Scanner in = new Scanner(System.in); //Scanner function for the variables
         System.out.print("How many gallons in your gas tank: "); //Output requesting the information for first variable
         double gas = in.nextDouble(); //Variable for gas
@@ -27,7 +27,7 @@ public class FuelCostCalculator {
         }
         double per100Miles = ((gasPrice*gas)/100); //This is figuring out how much it will cost per 100 miles
         System.out.println("Your cost per 100 miles is " + per100Miles);
-        double distanceTraveled = (milesPerGallon*gas*100); //This is figuring out the distance and mpg and then multiply it all by 100 to get the distance
+        double distanceTraveled = ((milesPerGallon/gas)*100); //This is figuring out the distance and mpg and then multiply it all by 100 to get the distance
         System.out.println("This is the distance you can travel with what is in your tank " + distanceTraveled);
 
 
